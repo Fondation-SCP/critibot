a1082789fe53e908bc06775d1b0808e9576b0477
Cyrielle <cyriellecentori@protonmail.com>
2024-11-11 14:22:37 +0100

    Changement des salons d'affichage et passage à la fondabots-lib v1.2.0

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,13 @@ use maplit::hashmap;
 use serenity::all::{ChannelId, GatewayIntents};
 
 use ecrit::{
-    Ecrit,
     fields::Status,
-    fields::Type
+    fields::Type,
+    Ecrit
 };
 
 mod ecrit;
 mod commands;
-
 pub type DataType = fondabots_lib::DataType<Ecrit>;
 
 #[tokio::main]
@@ -28,11 +27,8 @@ async fn main() {
             "./critibot.yml",
             commands::command_list(),
             vec![
-                Affichan::new(ChannelId::new(843956373103968308), Box::new(|ecrit| {
+                Affichan::new(ChannelId::new(1299620421506699275), Box::new(|ecrit| {
                     ecrit.status == Status::Ouvert || ecrit.status == Status::OuvertPlus
-                })),
-                Affichan::new(ChannelId::new(614947463610236939), Box::new(|ecrit| {
-                    ecrit.status == Status::OuvertPlus
                 })),
                 Affichan::new(ChannelId::new(896361827884220467), Box::new(|ecrit| {
                     ecrit.status == Status::Inconnu || ecrit.status == Status::Infraction
@@ -40,15 +36,9 @@ async fn main() {
                 Affichan::new(ChannelId::new(896362452818747412), Box::new(|ecrit| {
                     ecrit.type_ == Type::Autre
                 })),
-                Affichan::new(ChannelId::new(554998005850177556), Box::new(|ecrit| {
-                    (ecrit.status == Status::Ouvert || ecrit.status == Status::OuvertPlus) &&
-                    ecrit.tags.iter().fold(false, |acc, tag| {
-                        acc || tag.contains("Concours") || tag.contains("Validation") || tag.contains("Event")
-                    })
-                }))
             ],
             hashmap! {
-                "organichan" => 614947463610236939
+                "organichan" => 878917114474410004
             }
         ).await {
             Ok(mut bot) => if let Err(e) = bot.start().await {
